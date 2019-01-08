83a7b6eba93c26537203c437da62cbfd75db48a6
hitanga <hitanga@gmail.com>
2019-01-08 20:14:47 +0530

    added homepage
    added homepage to github

---
+++ js/main.js
@@ -0,0 +1,83 @@
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+new WOW().init();
+				$(window).scroll(function(){
+					$('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+				});
+
+
+				    $(window).on("scroll", function () {
+
+						var my_skill = ".about .skills .skill";
+						if ($(my_skill).length !== 0){
+								spy_scroll(my_skill); 
+						}
+
+						
+
+						//-- Scroll top --//
+						var scrollTop = $(".top");
+						if (scrollTop.length !== 0) {
+								if ($(window).scrollTop() >= $(".about").offset().top) {
+										scrollTop.addClass("scroll_top_show");
+								} else {
+										scrollTop.removeClass("scroll_top_show");
+								}
+						}
+
+						});
